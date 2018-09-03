d60ee825e5a8c428dff368ae847c84c9c01a227d
xuheng <444621944@qq.com>
2018-09-03 15:43:58 +0800

    完善用户登入与登出功能

--- models/users.js
+++ models/users.js
@@ -4,5 +4,14 @@ module.exports = {
     // 注册一个用户
     create: function create (user) {
         return User.create(user).exec()
+    },
+
+    // 通过用户名获取用户信息
+    getUserByName: function getUserByName(name) {
+        return User
+            .findOne({name: name})
+            // 使用addCreatedAt() 自定义插件（通过 _id 生成时间戳）
+            .addCreatedAt()
+            .exec()
     }
 }
